7fe96c1a0ee4f1eef8f687a80b001c60f90134b1
Sebastian Krzemień <59866137+skrzemien@users.noreply.github.com>
2022-03-15 16:55:07 +0100

    fdgdf

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = () => {
+const Header = ({title}) => {
+  const onClick =() =>{
+    console.log('click')
+  }
+
   return (
-    <div>
-      <h1>Task Tracker</h1>
-    </div>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
+    </header>
   )
 }
+
+
+Header.defaultProps = {
+  title: "Task Tracker",
+}
+
 
 export default Header
